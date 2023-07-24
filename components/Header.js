600c404319637d7aa960f2d071631cfe20be7fc8
Roba Nath Basnet <robanbth@gmail.com>
2023-07-24 11:52:16 +0600

    first commit

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className={StyleSheet.wrapper}>
+      <div className={styles.content}>
+        <div className={styles.logoContainer}>
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
