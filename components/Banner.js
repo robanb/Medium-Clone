7c9b93c781a86ffed9252bd3c12e2d53a3f2398f
Roba Nath Basnet <robanbth@gmail.com>
2023-07-25 09:42:47 +0600

    done: Header and Banner

--- components/Banner.js
+++ components/Banner.js
@@ -3,30 +3,32 @@ import Logo from "./../public/images/banner.png";
 
 const styles = {
   accentedButton: "bg-black text-white py-2 px-4 rounded-full",
-  content: " flex-1 flex items-center justify-between ",
-  wrapper:
-    "h-max[10rem] flex items-center justify-center bg-[#FCC017] border-y border-black py-5",
-  logo: " md:inline-flex object-contain ",
+  content: `max-w-7xl flex-1 flex items-center justify-between`,
+  wrapper: `h-max-[10rem] flex items-center justify-center bg-[#FCC017] border-y border-black`,
 };
 
 const Banner = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
-        <div className="space-y-5 px-10">
+        <div className="space-y-5 px-10 flex-[3]">
           <h1 className="max-w-xl text-[6rem] font-mediumSerif">
-            Stay Curious
+            Stay Curious.
           </h1>
+
           <h3 className="text-2xl">
             Discover stories, thinking, and expertise from writers on any topic.
           </h3>
+
           <button className={styles.accentedButton}>Start Reading</button>
         </div>
+
         <Image
-          className={styles.logo}
-          src={Logo.src}
+          className="hidden h-32 md:inline-flex object-contain flex-1"
+          src={Logo}
           width={500}
           height={400}
+          alt="banner"
         />
       </div>
     </div>
--- components/Header.js
+++ components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
     <div className={styles.wrapper}>
       <div className={styles.content}>
         <div className={styles.logoContainer}>
-          <Image className={styles.logo} src={Logo} height={40} width={200} />
+          <Image className={styles.logo} src={Logo} height={40} width={200} alt="logo" />
         </div>
         <div className={styles.bannerNav}>
           <div>Our Story</div>
